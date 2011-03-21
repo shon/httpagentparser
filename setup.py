3a23433752042d3d260c1ed330575df92719ba21
Shekhar <pythonic@gmail.com>
2011-03-21 14:32:39 +0530

    docs

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='httpagentparser',
+    version='0.8.5',
+    description='Extracts OS Browser etc information from http user agent string',
+    long_description=file("README.rst").read(),
+    packages=find_packages(),
+    author='Shekhar Tiwatne',
+    author_email='pythonic@gmail.com',
+    url="http://flavors.me/shon",
+    license="http://www.opensource.org/licenses/mit-license.php",
+    )
+
