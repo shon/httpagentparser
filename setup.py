a9d09f72ea9430a4fb102b6fea9c7c59ac651366
Shekhar <pythonic@gmail.com>
2012-09-24 17:07:07 +0530

    version bump to cover recent changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='httpagentparser',
-    version='1.1.3',
+    version='1.1.4',
     url="https://github.com/shon/httpagentparser",
     classifiers=[
         'Programming Language :: Python',
