a68d93f425ab66fe9e9eb582a002a68e5a60bc78
Joe Ang <joang@zendesk.com>
2021-01-12 12:10:50 +0800

    Shallow copy globals()

--- httpagentparser/__init__.py
+++ httpagentparser/__init__.py
@@ -31,7 +31,8 @@ class DetectorsHub(dict):
         return iter(self._known_types)
 
     def registerDetectors(self):
-        detectors = [v() for v in globals().values() if DetectorBase in getattr(v, '__mro__', [])]
+        globals_copy = globals().copy()
+        detectors = [v() for v in globals_copy.values() if DetectorBase in getattr(v, '__mro__', [])]
         for d in detectors:
             if d.can_register:
                 self.register(d)
